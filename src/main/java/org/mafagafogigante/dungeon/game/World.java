d612c091506d7043c51793b8eabcc71aa817b499
Fatih Furkan HAS <ffatihhas@gmail.com>
2017-03-26 11:27:58 +0300

    World.java dosyasinda home location'i eklenmesi icin gerekli degisiklikler yapilmistir

--- src/main/java/org/mafagafogigante/dungeon/game/World.java
+++ src/main/java/org/mafagafogigante/dungeon/game/World.java
@@ -85,16 +85,18 @@ public class World implements Serializable {
    * Adds a Location to this World.
    */
   public void addLocation(Location locationObject, Point coordinates) {
-    if (locations.containsKey(coordinates)) {
-      throw new IllegalStateException("tried to repeatedly add a location to " + coordinates + ".");
-    }
-    if (!locationObject.getWorld().equals(this)) {
-      World world = locationObject.getWorld();
-      throw new IllegalStateException("tried to add location with World field " + world + " to " + this + ".");
-    }
-    if (!locationObject.getPoint().equals(coordinates)) {
-      Point point = locationObject.getPoint();
-      throw new IllegalStateException("tried to add location with Point field " + point + " to " + coordinates + ".");
+    if (!locationObject.toString().equals("Home")) {
+      if (locations.containsKey(coordinates)) {
+        throw new IllegalStateException("tried to repeatedly add a location to " + coordinates + ".");
+      }
+      if (!locationObject.getWorld().equals(this)) {
+        World world = locationObject.getWorld();
+        throw new IllegalStateException("tried to add location with World field " + world + " to " + this + ".");
+      }
+      if (!locationObject.getPoint().equals(coordinates)) {
+        Point point = locationObject.getPoint();
+        throw new IllegalStateException("tried to add location with Point field " + point + " to " + coordinates + ".");
+      }
     }
     locations.put(coordinates, locationObject);
     worldStatistics.addLocation(locationObject.getName().getSingular());
